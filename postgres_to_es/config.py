678b287fc9d39f3a3251be974086ef6add00887d
SergeyP1990 <totalmeltdown90@gmail.com>
2022-01-13 00:28:33 +0300

    config
    добавил конфиг и его парсинг

--- postgres_to_es/config.py
+++ postgres_to_es/config.py
@@ -1,6 +1,33 @@
 import logging
-import os
-import configparser
+import typing
+import toml
 from pydantic import BaseModel
 
+class PostgresConfig(BaseModel):
+    name: str
+    user: str
+    password: str
+    address: str
+    port: int
 
+
+class ElasticConfig(BaseModel):
+    address: str
+    port: int
+
+
+class BackoffConfig(BaseModel):
+    start_sleep_time: float
+    factor: int
+    border_sleep_time: float
+
+
+class Config(BaseModel):
+    pg_database: PostgresConfig
+    elastic: ElasticConfig
+    backoff: BackoffConfig
+
+    @classmethod
+    def parse_config(cls, file_path):
+        conf = toml.load(file_path)
+        return cls.parse_obj(conf)
