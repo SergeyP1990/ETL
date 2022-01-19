e4f3dede0fba29d9e502c3f9d4a935bf957fc21a
Sergey Plakunov <s_plakunov@eureca.ru>
2022-01-19 19:15:04 +0300

    Nested sqls and class joining

--- postgres_to_es/data_representation.py
+++ postgres_to_es/data_representation.py
@@ -1,6 +1,7 @@
 import datetime
 import uuid
 from dataclasses import dataclass, field, InitVar
+from typing import List
 
 @dataclass
 class NotNullChecker:
@@ -13,23 +14,8 @@ class NotNullChecker:
 @dataclass
 class BaseRecord:
     id: uuid.UUID = field(default_factory=uuid.uuid4)
-    created_at: datetime.datetime = field(default=None)
+    # created_at: datetime.datetime = field(default=None)
     updated_at: datetime.datetime = field(default=None)
-
-
-@dataclass
-class FilmWork(BaseRecord, NotNullChecker):
-    title: str = field(default=None)
-    type: str = field(default=None)
-    description: str = field(default=None)
-    creation_date: datetime.date = field(default=None)
-    certificate: str = field(default=None)
-    file_path: str = field(default=None)
-    rating: float = field(default=None)
-    table_name: InitVar[str] = "film_work"
-
-    def __post_init__(self, table_name: str):
-        self._check_not_null_(("title", "type"))
 
 
 @dataclass
@@ -54,26 +40,15 @@ class Person(BaseRecord, NotNullChecker):
 
 
 @dataclass
-class GenreFilmWork(NotNullChecker):
-    film_work_id: uuid.UUID
-    genre_id: uuid.UUID
-    id: uuid.UUID = field(default_factory=uuid.uuid4)
-    created_at: datetime.datetime = field(default=None)
-    table_name: InitVar[str] = "genre_film_work"
-
-    def __post_init__(self, table_name: str):
-        self._check_not_null_(("film_work_id", "genre_id"))
-
-@dataclass
-class PersonFilmWork(NotNullChecker):
-    film_work_id: uuid.UUID
-    person_id: uuid.UUID
-    role: str
-    id: uuid.UUID = field(default_factory=uuid.uuid4)
-    created_at: datetime.datetime = field(default=None)
-    table_name: InitVar[str] = "person_film_work"
-
-    def __post_init__(self, table_name: str):
-        self._check_not_null_(("film_work_id", "person_id", "role"))
-
-
+class FilmWork(BaseRecord, NotNullChecker):
+    fw_id: uuid.UUID = field(default_factory=uuid.uuid4)
+    imdb_rating: float = field(default=None)
+    title: str = field(default=None)
+    description: str = field(default=None)
+    genres: List[Genre] = field(default_factory=list)
+    director: list = field(default_factory=list)
+    actors_names: List = field(default_factory=list)
+    writers_names: List = field(default_factory=list)
+    actors: List = field(default_factory=list)
+    writers: List = field(default_factory=list)
+    table_name: InitVar[str] = "film_work"
